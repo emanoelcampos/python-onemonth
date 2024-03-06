963c7d7ec644dc5e81043423b0c8e1d55deff66a
emanoelcampos <emanoel.cv@gmail.com>
2024-03-06 18:58:31 -0300

    add lesson 13 - variables

---
+++ one-month-course/week-1/lesson-13.py
@@ -0,0 +1,10 @@
+# Variables are just nicknames
+# they're plain, lowercase words
+# examples: x, musics, names, phone_a_quail
+
+name = "Emanoel Campos"
+orphan_fee = 190
+teddy_bear_fee = 130.70
+
+total = orphan_fee + teddy_bear_fee
+print(name, "the total will be", total)
