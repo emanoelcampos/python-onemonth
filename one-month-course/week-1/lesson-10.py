c94eebc5ca6b75a8ffa31e261a2738025de67d11
emanoelcampos <emanoel.cv@gmail.com>
2024-03-06 16:57:21 -0300

    add lesson 10 py - writing print functions

---
+++ one-month-course/week-1/lesson-10.py
@@ -0,0 +1,19 @@
+print("I'm learning Python!")
+
+print("You are still learning Python", "Emanoel")
+
+# print "This only works with Python 2" | This code does not work with Python 3!
+
+print("A conga está chamando")
+print("Vamos todos até lá")
+print("Pois a tamba está tocando")
+print("Hoje nós vamos sambar")
+print("Ié, ié, ié, ié, ié, iá")
+print("Ié, ié, ié, ié, ié, iá")
+
+print("Desde que se foi")
+print("O nosso Rei Nagô, ô, ô")
+print("Ninguém jamais fez samba")
+print("Ninguém jamais cantou")
+print("Ô, ô, ô, ô")
+print("Um lamento ou uma canção de amor")
