c6263a1f588005184ccbe62a56d2f66fd5ea6242
emanoelcampos <emanoel.cv@gmail.com>
2024-03-07 19:04:29 -0300

    add lesson 18 - getting user input

---
+++ one-month-course/week-1/lesson-18.py
@@ -0,0 +1,8 @@
+user_name = input("What's your name? ")
+user_age = int(input("How old are you? "))
+user_age_in_dog_years = user_age * 7
+# user_age_in_dog_years = int(user_age) * 7
+
+print(f"{user_name} you are {user_age} years old.")
+print(f"{user_name} you are {user_age_in_dog_years} in dog years.")
+
