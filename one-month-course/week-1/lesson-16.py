408171617b83488fa7a6ddbbe9af3ae06a32684a
emanoelcampos <emanoel.cv@gmail.com>
2024-03-07 00:08:53 -0300

    add lesson 16 - string examples

---
+++ one-month-course/week-1/lesson-16.py
@@ -0,0 +1,14 @@
+# Strings are text surrounded by quotes
+# Both single (') or double (") or triple (""") quotes are used
+# examples: "bikes", '1997', "Im learning python"
+
+kendrick_quote_double = "I was slightly confused. Guess I'm not mature as I think, got some healing to do."
+print(kendrick_quote_double)
+
+kendrick_quote_triple = """I was slightly confused. 
+Guess I'm not mature as I think, 
+got some healing to do."""
+print(kendrick_quote_triple)
+
+criolo_quote = 'Pois quem não vive em verdade, meu bem, "flutua nas ilusões da mente de um \'louco\' qualquer."'
+print(criolo_quote)
