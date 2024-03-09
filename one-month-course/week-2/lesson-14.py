e2e5d392ca511875afc43d7e838c84a80cde65fc
emanoelcampos <emanoel.cv@gmail.com>
2024-03-09 16:41:10 -0300

    add lesson 14 - for loop challenge using range

---
+++ one-month-course/week-2/lesson-14.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for number in numbers:
+    squared_number = number * number
+    print(f"{number} x {number} = {squared_number}")
+
+print("------------------------------------------------------")
+
+for number1 in range(1, 11):
+    print(number1, "squared is", number1 ** 2)
