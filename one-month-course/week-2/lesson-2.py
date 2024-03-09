cf1b23212d865670b2953a6b5eb4751fdeb06822
emanoelcampos <emanoel.cv@gmail.com>
2024-03-09 03:00:52 -0300

    add lesson 2 - introduction to conditional statements

---
+++ one-month-course/week-2/lesson-2.py
@@ -0,0 +1,9 @@
+answer = input("Do you want to hear a joke?\n")
+
+if answer == "Yes":
+    print("I'm against picketing, but i don't know how to show it.")
+
+if True:
+    # Anythng here will run
+if False:
+    # This will never run
