1a26ba76925291a898053edc393f1799a0220da5
emanoelcampos <emanoel.cv@gmail.com>
2024-03-08 22:41:52 -0300

    Updated the print statement for the total bill tip

--- one-month-course/week-1/lesson-20.py
+++ one-month-course/week-1/lesson-20.py
@@ -1,14 +1,12 @@
 print("Tip Calculator\n")
 
-total_bill = input("What is your total bill?\n")
-
-base_bill_amount = float(total_bill.replace("$", ""))
+total_bill = float(input("What is your total bill?\n").replace("$", ""))
 
 print("The total price of your account will be based on these three standard tip percentages: 15%, 18% or 20%\n")
 
-bill_with_15_percent_tip = base_bill_amount * (1 + 0.15)
-bill_with_18_percent_tip = base_bill_amount * (1 + 0.18)
-bill_with_20_percent_tip = base_bill_amount * (1 + 0.20)
+bill_with_15_percent_tip = total_bill * (1 + 0.15)
+bill_with_18_percent_tip = total_bill * (1 + 0.18)
+bill_with_20_percent_tip = total_bill * (1 + 0.20)
 
 print(f"Total bill with 15% tip: {bill_with_15_percent_tip:.2f}\n")
 print(f"Total bill with 18% tip: {bill_with_18_percent_tip:.2f}\n")
