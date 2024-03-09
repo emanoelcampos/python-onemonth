830db8d2895d876082541a133be84dd070020a96
emanoelcampos <emanoel.cv@gmail.com>
2024-03-09 15:32:35 -0300

    add lesson 10 - improvement refactoring

---
+++ one-month-course/week-2/lesson-10.py
@@ -0,0 +1,8 @@
+answer = input("Do you want to hear a joke?\n")
+
+if "y" in answer.lower():
+    print("I'm against picketing, but i don't know how to show it.")
+elif "n" in answer.lower():
+    print("Fine")
+else:
+    print("I don't understand.")
