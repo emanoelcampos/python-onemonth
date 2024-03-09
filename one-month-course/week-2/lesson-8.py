477b4bfcc1af9832dfa214ead75ab14e2565e664
emanoelcampos <emanoel.cv@gmail.com>
2024-03-09 15:22:41 -0300

    add lesson 8 - or operator

---
+++ one-month-course/week-2/lesson-8.py
@@ -0,0 +1,8 @@
+answer = input("Do you want to hear a joke?\n")
+
+if answer == "Yes" or answer == "yes":
+    print("I'm against picketing, but i don't know how to show it.")
+elif answer == "No" or answer == "no":
+    print("Fine")
+else:
+    print("I don't understand.")
