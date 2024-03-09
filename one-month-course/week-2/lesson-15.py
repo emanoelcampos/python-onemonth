af706145aa5efc7720c51dc850213806c0c5a36a
emanoelcampos <emanoel.cv@gmail.com>
2024-03-09 16:55:13 -0300

    add lesson 15 - accessing data in a list

---
+++ one-month-course/week-2/lesson-15.py
@@ -0,0 +1,16 @@
+# here's how you access elements of a list
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+
+first_animal = animals[0]
+print(first_animal)
+
+third_animal = animals[2]
+print(third_animal)
+
+print("There are this many animals:", len(animals))
+print("Animals is a:", type(animals))
+
+random_things = ["Puppies", 55, "Evinha", 1/2, ["Oh no!", "A list inside a list"]]
+another_list = random_things[-1]
+print(another_list)
+print(type(another_list))
