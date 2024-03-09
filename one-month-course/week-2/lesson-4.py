901d8f6355b6bce0982a56dd392f49d0add9f680
emanoelcampos <emanoel.cv@gmail.com>
2024-03-09 03:45:11 -0300

    add lesson 4 - elif and else

---
+++ one-month-course/week-2/lesson-4.py
@@ -0,0 +1,9 @@
+answer = input("Do you want to hear a joke?\n")
+
+if answer == "Yes":
+    print("I'm against picketing, but i don't know how to show it.")
+elif answer == "No":
+    print("Fine")
+else:
+    print("I don't understand.")
+
