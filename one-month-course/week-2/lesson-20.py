7759949e74287e4e062451a5094bdabe9ebed1e1
emanoelcampos <emanoel.cv@gmail.com>
2024-03-12 19:51:55 -0300

    add lesson 20 - functions

---
+++ one-month-course/week-2/lesson-20.py
@@ -0,0 +1,21 @@
+def greet(name):
+    return f"Hey {name}!"
+
+
+def concatenate(word_one, word_two):
+    return word_one + word_two
+
+
+def age_in_dog_years(age):
+    return age * 7
+
+
+print(greet("emanoel"))
+print(greet("ana"))
+
+my_name = "emanoel"
+print(greet(my_name))
+
+print(concatenate("emanoel", "campos"))
+
+print(age_in_dog_years(27))
